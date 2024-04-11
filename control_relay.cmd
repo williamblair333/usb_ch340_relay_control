ddf36783cff9c50b2c3e5884b693a4232fc23ba8
William Blair <60581672+williamblair333@users.noreply.github.com>
2024-04-11 11:46:08 -0400

    Add files via upload

---
+++ control_relay.cmd
@@ -0,0 +1,3 @@
+
+@echo off
+python relay_control.py --state ON --mode LOCAL --src-dir "C:\path\to\scripts" --device COM12 --relay 1 --timeout 5
---
+++ control_relay.sh
@@ -0,0 +1,3 @@
+
+#!/bin/bash
+python relay_control.py --state OFF --mode REMOTE --src-dir "/path/to/scripts" --device /dev/ttyUSB18 -r 1 -t 5 --user ubuntu --host 192.168.1.63
